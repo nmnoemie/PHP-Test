963cbd26bbd3a3bd84ecac3d84972b5cae023ef1
Noémie Niamien <noemienmn@gmail.com>
2026-05-12 22:11:38 +0200

    feat:comparaison

---
+++ test2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doc</title>
+</head>
+<body>
+    <?php
+
+var_dump(0 == "0");
+var_dump(0 === "0");
+var_dump("1" == 1);
+var_dump("1" === 1);
+var_dump(null == false);
+var_dump(null === false);
+
+
+
+    ?>
+</body>
+</html>
