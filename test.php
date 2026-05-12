67d25500cf6a8c94cca0f1eebea7368f7498d72b
Noémie Niamien <noemienmn@gmail.com>
2026-05-12 22:09:04 +0200

    feat:first exercice

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test page</title>
+</head>
+<body>
+    <?php 
+$prenom="Marie"  ;         
+$anneeNaissance=2018 ;
+$annee=2026 ;                 
+
+// Affichage
+echo "Marie a " .$annee-$anneeNaissance . " ans en 2026"; 
+
+
+?>
+</body>
+</html>
