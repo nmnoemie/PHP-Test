6d63b55eb9fc20ac80e9500390cbaa5f98fb40d2
Noémie Niamien <noemienmn@gmail.com>
2026-05-12 22:10:25 +0200

    feat:temperature

---
+++ test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $celsius =20;
+    $fahrenheit= $celsius*1.8+32;
+    echo "20°C =".$fahrenheit."°F" ;
+    ?>
+</body>
+</html>
